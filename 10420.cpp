decdb6ef70ec4a7389f596c0a2f76016852ddfdd
sh00nya <asitmahato@sh00nya.in>
2012-12-28 04:43:11 -0800

    solved

---
+++ 10420.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <sstream>
+#include <cmath>
+#include <cstdio>
+#include <deque>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+  int T;
+	cin >> T;
+	map<string,int> M;
+	for( int i=0; i<T; ++i )
+	{
+		string line;
+		if( i == 0 )
+			cin.ignore();
+		getline( cin, line );
+		int size = line.size();
+		string country;
+		for( int j=0; j<size; ++j )
+		{
+			if( line[j] == ' ' )
+			{
+				country = line.substr( 0, j );
+				++M[country];
+				break;
+			}
+		}
+	}
+	for( map<string,int>::iterator it=M.begin(); it!=M.end(); ++it )
+		cout << it->first << " " << it->second << endl;
+}
